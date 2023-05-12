f9cbe382fa5e0d5fc17f796dba624f714aa15586
Somraj Chowdhury <chowdhury.somraj08@gmail.com>
2023-05-12 22:05:59 +0530

    Created user registration api view and url

---
+++ src/authentication/urls.py
@@ -0,0 +1,11 @@
+"""URL patterns for authentication application."""
+
+
+from django.urls import path
+from .views import RegisterView
+
+
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
+]
+
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+"""API view classes for authentication application."""
 
-# Create your views here.
+
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import UserSerializer
+
+
+class RegisterView(generics.GenericAPIView):
+    """Add new users to database."""
+
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        """Create and save new user object."""
+        user_data = request.data
+        serializer = self.serializer_class(data=user_data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        serialized_user_data = serializer.data
+        return Response(serialized_user_data, status=status.HTTP_201_CREATED)
+
