e8746c3192dae6bc1f6140abddd4936e466ae209
Somraj Chowdhury <chowdhury.somraj08@gmail.com>
2023-05-29 00:01:39 +0530

    Added JWT auth class setting for DRF

--- src/incomeexpenseapi/settings.py
+++ src/incomeexpenseapi/settings.py
@@ -104,3 +104,10 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'Income Expense API <noreply@incomeexpenseapi.com>'
 
+# Rest framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
